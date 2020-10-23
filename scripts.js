7bd7ccb50c115b85439e5353ba9b9c7bcc4022ae
Serena Hasler <srhasler@yahoo.com>
2020-10-23 15:44:11 -0500

    Insert JS code to cause image clicked upon to become main image

---
+++ scripts.js
@@ -0,0 +1,17 @@
+/**
+ * TODO: Whenever we click on an image, take the src and alt and replace the 'main display' with the src alt
+ *  */
+
+// JS Object
+const currentImageAttrs = {};
+
+// Use dot notation to access properties and functions/methods
+const displayImage = document.querySelector("#display img");
+const fsImages = document.querySelectorAll("#film-strip img");
+
+fsImages.forEach(function (fsImage) {
+  fsImage.addEventListener("click", function (event) {
+    displayImage.src = event.target.src;
+    displayImage.alt = event.target.alt;
+  });
+});
